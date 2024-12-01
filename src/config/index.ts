f4162b7f2c0fb39fb0e6e29cd171fc8f80b55c25
birhanu-dejen <birhanudejen617@gmail.com>
2024-12-01 21:17:54 +0300

    use mongodb atlas

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,6 +10,7 @@ export default {
   pageSize: Number(process.env.PAGE_SIZE),
   saltRound: Number(process.env.SALT_ROUND),
   databaseURL: `${process.env.DATABASE_URL}`,
-  ChapaSecretKey:`${process.env.CHAPA_SECRET_KEY}`,
+  ChapaSecretKey: `${process.env.CHAPA_SECRET_KEY}`,
   catagories: ["Man", "Woman"],
+  atlas: `${process.env.DATABASE}`,
 };
--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -3,6 +3,7 @@ import * as mongoose from "mongoose";
 import config from "../config/index";
 
 export default async () => {
-  const connection = await mongoose.connect(config.databaseURL);
+  //const connection = await mongoose.connect(config.databaseURL);
+  const connection = await mongoose.connect(config.atlas);
   return connection.connection.db;
 };
